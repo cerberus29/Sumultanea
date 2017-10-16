3f98af4bfa65a63c4b93b82cd8d5fa60a886b057
Dominic Lemire <dominic.lemire@broadcom.com>
2017-10-16 15:51:29 -0700

    Explained character selection

--- app/src/main/java/com/example/cj/sumultanea/CharacterSelectionActivity.java
+++ app/src/main/java/com/example/cj/sumultanea/CharacterSelectionActivity.java
@@ -4,13 +4,11 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class CharacterSelectionActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    public static final String TAG = "CharacterSel";
     private MediaPlayer mediaPlayer;
     CharacterSelectionAdapter mAdapter;
     private GridView gridview;
@@ -19,7 +17,10 @@ public class CharacterSelectionActivity extends AppCompatActivity implements Ada
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
-        // Automatically fill-up the GridView with our adapter
+
+        // Instead of manually filling the grid with character icons, we connect
+        // the GridView to an "adapter" whose job is to prepare each character
+        // icon in a uniform way.
         gridview = findViewById(R.id.characterSelectionGridView);
         mAdapter = new CharacterSelectionAdapter(this, R.layout.character_selection_example_item, CharacterPool.charactersList);
         gridview.setAdapter(mAdapter);
@@ -41,22 +42,26 @@ public class CharacterSelectionActivity extends AppCompatActivity implements Ada
         mediaPlayer.release();
     }
 
+    // Called when the player clicks an item in the grid view (position)
     @Override
     public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        // Launch CharacterDetailsActivity to show the details of the character at "position"
         Intent intent;
-        Log.d(TAG, "Launching character details activity");
         intent = new Intent(this, CharacterDetailsActivity.class);
         intent.putExtra(simultanea.CHARACTER_KEY, position);
         startActivityForResult(intent, simultanea.PICK_CHARACTER_REQUEST);
     }
 
+    // Called when the CharacterDetailsActivity returns:
+    // If the user clicked "pick" in the details activity, we go back to the main menu.
+    // If the user clicked the back button, we stay here to select another character.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case simultanea.PICK_CHARACTER_REQUEST:
                 if (resultCode == RESULT_OK) {
+                    // Send the character number back to the main activity
                     int character = data.getIntExtra(simultanea.CHARACTER_KEY, gridview.getSelectedItemPosition());
-                    // Return the character selection back to the main activity
                     Intent resultIntent = new Intent();
                     resultIntent.putExtra(simultanea.CHARACTER_KEY, character);
                     setResult(RESULT_OK, resultIntent);
--- app/src/main/java/com/example/cj/sumultanea/CharacterSelectionAdapter.java
+++ app/src/main/java/com/example/cj/sumultanea/CharacterSelectionAdapter.java
@@ -12,29 +12,39 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/* This class provides character tiles to the GridView */
 class CharacterSelectionAdapter extends ArrayAdapter<Character> {
     private final Context mContext;
     private final int mResource;
     private LayoutInflater mLayoutInflater = null;
 
+    // We initialize from the list of characters
     CharacterSelectionAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull Character[] objects) {
         super(context, resource, objects);
         mContext = context;
         mResource = resource;
     }
 
+    // This is called by the GridView to request the image/name for a
+    // specific character (position) in our list.
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        View view = null; // = convertView;
+        // Make sure the character at "position" exists in our list
+        Character character = getItem(position);
+        assert character != null;
+
+        // Create a small view using our template layout "character_selection_example_item"
+        View view = null; // = convertView; TODO: figure-out why animations break when recycling convertView
         if (view == null) {
+            // Convert the XML template into a view object using the "layout inflater"
             if (mLayoutInflater == null) {
                 mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             }
             view = mLayoutInflater.inflate(mResource, parent, false);
         }
-        Character character = getItem(position);
-        assert character != null;
+
+        // Replace the image and name from the template by this specific character info
         ImageView imageView = view.findViewById(R.id.imageViewCharacter);
         imageView.setImageResource(character.getImageResource());
         AnimationDrawable anim = (AnimationDrawable) imageView.getDrawable();
