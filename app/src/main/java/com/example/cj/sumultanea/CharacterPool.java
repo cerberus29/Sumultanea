639620d547457c3879eaa1f8224568bcf6f50058
Dominic Lemire <dominic.lemire@gmail.com>
2017-10-27 10:27:27 -0700

    Removed unused code

--- app/src/main/java/com/example/cj/sumultanea/CharacterPool.java
+++ app/src/main/java/com/example/cj/sumultanea/CharacterPool.java
@@ -1,12 +1,6 @@
 package com.example.cj.sumultanea;
 
 public class CharacterPool {
-    public static final int CHARACTER_PANDA = 0;
-    public static final int CHARACTER_DUEL = 1;
-    public static final int CHARACTER_PUMP = 2;
-    public static final int CHARACTER_WAR = 3;
-    public static final int CHARACTER_DEFAULT = CHARACTER_PUMP;
-
     public static final Character[] charactersList = {
             new Character(
                     R.string.bluepanda_name,
