eb64a82e0f842c32b84b2d830dd2aadbd39f249e
CJ <christianjerom29@gmail.com>
2017-10-27 21:34:36 +0800

    added Lore

--- app/src/main/java/com/example/cj/sumultanea/CharacterPool.java
+++ app/src/main/java/com/example/cj/sumultanea/CharacterPool.java
@@ -14,8 +14,6 @@ public class CharacterPool {
                     R.drawable.bluepanda_death,
                     R.drawable.bluepanda_attack,
                     R.string.Panda_Lore,
-
-
             new Character(
                     R.string.duel_name,
                     R.drawable.duel,
@@ -23,7 +21,6 @@ public class CharacterPool {
                     R.drawable.duel_death,
                     R.drawable.duel_attack,
                     R.string.Duelist_Lore,
-
             new Character(
                     R.string.pump_name,
                     R.drawable.pump,
@@ -31,13 +28,11 @@ public class CharacterPool {
                     R.drawable.pump_death,
                     R.drawable.pump_attack,
                     R.string.Scarecrow_Lore,
-
             new Character(R.string.war_name,
                     R.drawable.war,
                     R.drawable.war_hurt,
                     R.drawable.war_death,
                     R.drawable.war_attack,
                     R.string.Warrior_Lore,
-
     };
 }
